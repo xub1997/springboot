b5ad7a664fc60a24f6500192f7ac661cf1626dba
qingxu_L <836192293@qq.com>
2022-03-13 21:16:37 +0800

    fix: 修改pom依赖及部分代码问题

--- freemarker/src/main/java/com/xub/freemarker/FreeMakerApplication.java
+++ freemarker/src/main/java/com/xub/freemarker/FreeMakerApplication.java
@@ -1,13 +1,17 @@
-package com.xub.hotdeploy;
+package com.xub.freemarker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author liqingxu
+ * @Description
+ * @create 2022-03-13
+ */
 @SpringBootApplication
-public class HotdeployApplication {
+public class FreeMakerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HotdeployApplication.class, args);
+        SpringApplication.run(FreeMakerApplication.class, args);
     }
-
 }
--- freemarker/src/main/java/com/xub/freemarker/controller/GeneratorController.java
+++ freemarker/src/main/java/com/xub/freemarker/controller/GeneratorController.java
@@ -1,10 +1,8 @@
 package com.xub.freemarker.controller;
 
-import com.sun.deploy.net.HttpResponse;
 import com.xub.freemarker.entity.Student;
 import com.xub.freemarker.utils.FreeMakerUtil;
 import freemarker.template.TemplateException;
-import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestContextHolder;
