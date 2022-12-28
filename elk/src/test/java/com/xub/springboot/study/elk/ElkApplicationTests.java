948c22f39da347d148d18b89273a22528562a39e
qingxu_L <836192293@qq.com>
2022-12-28 09:46:58 +0800

    chore: elk与SpringBoot日志集成

---
+++ elk/src/test/java/com/xub/springboot/study/elk/ElkApplicationTests.java
@@ -0,0 +1,14 @@
+package com.xub.springboot.study.elk;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
